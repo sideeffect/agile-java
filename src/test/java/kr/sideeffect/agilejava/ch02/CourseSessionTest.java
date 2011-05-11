ed742b649cf6ba3e0361ccd2a5bde934e6b71324
Outsider <outsideris@gmail.com>
2011-05-11 22:12:56 +0900

    page 92

--- src/test/java/kr/sideeffect/agilejava/ch02/CourseSessionTest.java
+++ src/test/java/kr/sideeffect/agilejava/ch02/CourseSessionTest.java
@@ -6,12 +6,19 @@ import java.util.ArrayList;
 
 import kr.sideeffect.agilejava.ch01.Student;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class CourseSessionTest {
+	private CourseSession session;
+	
+	@Before
+	public void setUp() {
+		session = new CourseSession("ENGL", "101");
+	}
+	
 	@Test
 	public void testCreate() {
-		CourseSession session = new CourseSession("ENGL", "101");
 		assertEquals("ENGL", session.getDepartment());
 		assertEquals("101", session.getNumber());
 		assertEquals(0, session.getNumberOfStudents());
@@ -19,8 +26,6 @@ public class CourseSessionTest {
 	
 	@Test
 	public void testEnrollStudents() {
-		CourseSession session = new CourseSession("ENGL", "101");
-		
 		Student student1 = new Student("Cain DiVoe");
 		session.enroll(student1);
 		assertEquals(1, session.getNumberOfStudents());
@@ -28,7 +33,7 @@ public class CourseSessionTest {
 		assertEquals(1, allStudents.size());
 		assertEquals(student1, allStudents.get(0));
 		
-		Student student2 = new Student("Cain DiVoe");
+		Student student2 = new Student("Coralee DeVaughn");
 		session.enroll(student2);
 		assertEquals(2, session.getNumberOfStudents());
 		assertEquals(2, allStudents.size());
