293eeba5d7c4e275a6ccff823d88719919511836
Outsider <outsideris@gmail.com>
2011-05-11 21:12:39 +0900

    page 75

---
+++ src/main/java/kr/sideeffect/agilejava/ch02/CourseSession.java
@@ -0,0 +1,20 @@
+package kr.sideeffect.agilejava.ch02;
+
+public class CourseSession {
+	private String department;
+	private String number;
+	
+	public CourseSession(String department, String number) {
+		this.department = department;
+		this.number = number;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+	
+}
---
+++ src/test/java/kr/sideeffect/agilejava/ch02/CourseSessionTest.java
@@ -0,0 +1,14 @@
+package kr.sideeffect.agilejava.ch02;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class CourseSessionTest {
+	@Test
+	public void testCreate() {
+		CourseSession session = new CourseSession("ENGL", "101");
+		assertEquals("ENGL", session.getDepartment());
+		assertEquals("101", session.getNumber());
+	}
+}
