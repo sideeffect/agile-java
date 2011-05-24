eaf75599191cb9eefd008bb874664dd6b861807b
Oustider <outsideris@gmail.com>
2011-05-24 22:04:03 +0900

    page 178

--- src/test/java/kr/sideeffect/agilejava/ch05/CourseReportTest.java
+++ src/test/java/kr/sideeffect/agilejava/ch05/CourseReportTest.java
@@ -20,8 +20,17 @@ public class CourseReportTest {
 		report.add(CourseSession.create("ENGL", "101", date));
 		report.add(CourseSession.create("CZEC", "200", date));
 		report.add(CourseSession.create("ITAL", "410", date));
+		report.add(CourseSession.create("CZEC", "220", date));
+		report.add(CourseSession.create("ITAL", "330", date));
 		
-		assertEquals("CZEC 200" + NEWLINE + "ENGL 101" + NEWLINE + "ITAL 410" + NEWLINE, report.text());
+		assertEquals(
+				"CZEC 200" + NEWLINE + 
+				"CZEC 220" + NEWLINE + 
+				"ENGL 101" + NEWLINE + 
+				"ITAL 330" + NEWLINE +
+				"ITAL 410" + NEWLINE, 
+				report.text()
+		);
 	}
 	
 	@Test
@@ -75,6 +84,10 @@ public class CourseReportTest {
 		
 		CourseSession sessionC = CourseSession.create("CMSC", "101", date);
 		assertEquals(0, sessionA.compareTo(sessionC));
+		
+		CourseSession sessionD = CourseSession.create("CMSC", "210", date);
+		assertTrue(sessionC.compareTo(sessionD) < 0);
+		assertTrue(sessionD.compareTo(sessionC) > 0);
 		
 	}
 }
