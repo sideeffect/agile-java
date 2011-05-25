470d5447c8706acc0c3bf8d278825e55737cbcb2
Oustider <outsideris@gmail.com>
2011-05-25 21:31:17 +0900

    page 186

--- src/main/java/kr/sideeffect/agilejava/ch01/Student.java
+++ src/main/java/kr/sideeffect/agilejava/ch01/Student.java
@@ -3,12 +3,14 @@ package kr.sideeffect.agilejava.ch01;
 import java.util.ArrayList;
 
 public class Student {
+	enum Grade { A, B, C, D, F };
+	
 	static final int CREDITS_REQUIRED_FOR_FULL_TIME = 12;
 	public static final String IN_STATE = "CO";
 	private String name;
 	private int credits;
 	private String state = "";
-	private ArrayList<String> grades = new ArrayList<String>();
+	private ArrayList<Grade> grades = new ArrayList<Grade>();
 
 	public Student(String name) {
 		this.name = name;
@@ -39,7 +41,7 @@ public class Student {
 		this.state = state;
 	}
 
-	public void addGrade(String grade) {
+	public void addGrade(Grade grade) {
 		grades.add(grade);
 	}
 
@@ -48,17 +50,17 @@ public class Student {
 			return 0.0;
 		}
 		double total = 0.0;
-		for (String grade : grades) {
+		for (Grade grade : grades) {
 			total += gradePointsFor(grade);
 		}
 		return total / grades.size();
 	}
 
-	private int gradePointsFor(String grade) {
-		if (grade.equals("A"))  return 4;
-		if (grade.equals("B")) return 3;
-		if (grade.equals("C")) return 2;
-		if (grade.equals("D")) return 1;
+	private int gradePointsFor(Grade grade) {
+		if (grade == Grade.A)  return 4;
+		if (grade == Grade.B) return 3;
+		if (grade == Grade.C) return 2;
+		if (grade == Grade.D) return 1;
 		return 0;
 	}
 }
