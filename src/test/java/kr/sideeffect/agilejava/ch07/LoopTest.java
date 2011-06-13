66efbc56aa756e30fc3568a9a209bf8c8dedc783
Oustider <outsideris@gmail.com>
2011-06-13 21:26:49 +0900

    page 245

--- src/test/java/kr/sideeffect/agilejava/ch07/LoopTest.java
+++ src/test/java/kr/sideeffect/agilejava/ch07/LoopTest.java
@@ -105,4 +105,25 @@ public class LoopTest {
 		}
 		return builder.toString();
 	}
+	
+	@Test
+	public void testEndTrim() {
+		assertEquals("", endTrim(""));
+		assertEquals(" x", endTrim(" x "));
+		assertEquals("y", endTrim("y"));
+		assertEquals("xaxa", endTrim("xaxa"));
+		assertEquals("", endTrim(" "));
+		assertEquals("xxx", endTrim("xxx    "));
+	}
+
+	public String endTrim(String source) {
+		int i = source.length()-1;
+		while (i >= 0) {
+			if (source.charAt(i) != ' ') {
+				break;
+			}
+			i--;
+		}
+		return source.substring(0, i + 1);
+	}
 }
